edaaae6041fe57ee7917991f7087ef0d3ab9dcc7
Rei-3 <christan.endam@norsu.edu.ph>
2025-07-10 22:13:49 +0800

    hello again

---
+++ App.ts
@@ -0,0 +1,10 @@
+console.log("Hello, world!");
+console.log("This is a simple TypeScript application.");
+console.log("Let's write some more code to see how it works.");
+//niggas
+console.log("WASSAP")
+
+var x:number = 5
+var y:string = "Hello"
+
+var z;
